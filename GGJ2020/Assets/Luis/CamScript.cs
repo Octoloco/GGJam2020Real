52d5c91b4c8f92c200b807e3c2821776f06b6b30
Luis Silva <luansimo@outlook.com>
2020-02-01 00:05:39 -0600

    Move update

---
+++ GGJ2020/Assets/Luis/CamScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScript : MonoBehaviour
+{
+    private Vector3 localZ;
+    public GameObject Cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Cube.transform.position);
+        transform.SetPositionAndRotation((Cube.transform.position - new Vector3(0.0f, 0.0f, 10.0f)), transform.rotation);
+    }
+}
---
+++ GGJ2020/Assets/Luis/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float tanSpeed;
+    public float angFrec;
+    public float amplitude;
+    private Vector3 moveDir;
+    private Vector3 localZ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDir = new Vector3(1.0f, 0.0f, 0.0f) * tanSpeed * Time.deltaTime;
+        localZ = (transform.position - Vector3.zero).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDir));
+        transform.Rotate(localZ, 5 * Mathf.Sin(5.0f * Time.time));
+    }
+}
