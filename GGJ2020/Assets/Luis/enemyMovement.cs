249ee318445ca2193b4963206db9acc898cf1e03
Luis Silva <luansimo@outlook.com>
2020-02-01 11:20:15 -0600

    Enemy Movement 1

--- GGJ2020/Assets/Luis/enemyMovement.cs
+++ GGJ2020/Assets/Luis/enemyMovement.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class enemyMovement : MonoBehaviour
 {
-    public float speed = 0.1f;
+    public float speed = 10.0f;
+    public float sineAmp = 0.1f;
+    public float sineFrec = 5.0f;
+    public float pathLen = 1.0f;
+    private List<GameObject> droids = new List<GameObject>();
+    private float count = 15.0f;
+    private Vector3 move;
+    private GameObject target;
     public enum Path
     {
         CIRCLE,
         SINE,
-        BNFORTH
+        BNFORTH,
+        CHASE
     }
     public Path enemyPath;
+    public Path defPath;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,21 +28,60 @@ public class enemyMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        switch(enemyPath)
+
+        if (droids.Count != 0)
+        {
+            enemyPath = Path.CHASE;
+        }
+        else
+        {
+            enemyPath = defPath;
+        }
+
+        switch (enemyPath)
         {
             case Path.CIRCLE:
-                transform.Translate(new Vector3(Mathf.Cos(Time.time), 0.0f , Mathf.Sin(Time.time))* speed);
+                move = new Vector3(sineFrec * Mathf.Cos(Time.time), 0.0f, Mathf.Sin(sineFrec * Time.time)) * 0.1f;
                 break;
             case Path.SINE:
-                transform.Translate(new Vector3(0.1f, 0.0f, 0.1f * Mathf.Sin(Time.time))* speed);
+                move = new Vector3(pathLen*Mathf.Sin(Time.time), 0.0f, sineAmp * Mathf.Sin(sineFrec*Time.time))*0.1f;
                 break;
             case Path.BNFORTH:
-                transform.Translate(new Vector3(0.0f, 0.0f, 0.1f*Mathf.Sin(Time.time))* speed);
+                move = new Vector3(0.0f, 0.0f, pathLen*Mathf.Sin(sineFrec * Time.time))*0.1f;
                 break;
-                
+            case Path.CHASE:
+                for (int i = 0; i < droids.Count; i++)
+                {
+                    float dist = Vector3.Distance(droids[i].transform.position, transform.position);
+
+                    if (dist < count)
+                    {
+                        target = droids[i];
+
+                    }
+                }
+
+                move = (target.transform.position - transform.position).normalized * speed * Time.deltaTime;
+                break;
         }
+        transform.Translate(move);
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "droid")
+        {
+            droids.Add(other.gameObject);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "droid")
+        {
+            droids.Remove(other.gameObject);
+            
+        }
     }
 }
