a7d1ee382a2d7a6da715ecd882b7215dc2a9a615
Luis Silva <luansimo@outlook.com>
2020-02-01 05:08:22 -0600

    enemy movement

--- GGJ2020/Assets/Luis/enemyMovement.cs
+++ GGJ2020/Assets/Luis/enemyMovement.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class enemyMovement : MonoBehaviour
 {
+    public float speed = 0.1f;
+    public enum Path
+    {
+        CIRCLE,
+        SINE,
+        BNFORTH
+    }
+    public Path enemyPath;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,19 @@ public class enemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch(enemyPath)
+        {
+            case Path.CIRCLE:
+                transform.Translate(new Vector3(Mathf.Cos(Time.time), 0.0f , Mathf.Sin(Time.time))* speed);
+                break;
+            case Path.SINE:
+                transform.Translate(new Vector3(0.1f, 0.0f, 0.1f * Mathf.Sin(Time.time))* speed);
+                break;
+            case Path.BNFORTH:
+                transform.Translate(new Vector3(0.0f, 0.0f, 0.1f*Mathf.Sin(Time.time))* speed);
+                break;
+                
+        }
         
     }
 }
