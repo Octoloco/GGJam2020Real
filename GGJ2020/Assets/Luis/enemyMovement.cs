f77b68fe90578e8a2631276fb5a13f7cb21ae5da
Luis Silva <luansimo@outlook.com>
2020-02-01 15:48:06 -0600

    AI

--- GGJ2020/Assets/Luis/enemyMovement.cs
+++ GGJ2020/Assets/Luis/enemyMovement.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class enemyMovement : MonoBehaviour
-{
+{ 
     public float speed = 10.0f;
-    public float sineAmp = 0.1f;
-    public float sineFrec = 5.0f;
-    public float pathLen = 1.0f;
+    public float rotSpeed = 10.0f;
+    public float pathLenF = 1.0f;
+    public float runTime = 1.0f;
+    public GameObject enemy;
+    bool avaliable = true;
     private List<GameObject> droids = new List<GameObject>();
     private float count = 15.0f;
     private Vector3 move;
     private GameObject target;
+    private bool colObst = false;
     public enum Path
     {
         CIRCLE,
         SINE,
         BNFORTH,
-        CHASE
+        CHASE,
+        DMG
     }
     public Path enemyPath;
     public Path defPath;
@@ -43,13 +47,28 @@ public class enemyMovement : MonoBehaviour
         switch (enemyPath)
         {
             case Path.CIRCLE:
-                move = new Vector3(sineFrec * Mathf.Cos(Time.time), 0.0f, Mathf.Sin(sineFrec * Time.time)) * 0.1f;
+                transform.Translate(Vector3.right*speed*Time.deltaTime);
+                transform.Rotate(Vector3.up, rotSpeed*Time.deltaTime);
                 break;
             case Path.SINE:
-                move = new Vector3(pathLen*Mathf.Sin(Time.time), 0.0f, sineAmp * Mathf.Sin(sineFrec*Time.time))*0.1f;
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+                transform.Rotate(Vector3.up, 3.14f * Mathf.Sin(rotSpeed*Time.time));
+                if (avaliable)
+                {
+                    StartCoroutine(Flip());
+                    avaliable = false;
+                }
+
+
                 break;
             case Path.BNFORTH:
-                move = new Vector3(0.0f, 0.0f, pathLen*Mathf.Sin(sineFrec * Time.time))*0.1f;
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+                if (avaliable) {
+                    StartCoroutine(Flip());
+                    avaliable = false;
+                }
+                
+
                 break;
             case Path.CHASE:
                 for (int i = 0; i < droids.Count; i++)
@@ -64,9 +83,12 @@ public class enemyMovement : MonoBehaviour
                 }
 
                 move = (target.transform.position - transform.position).normalized * speed * Time.deltaTime;
+                transform.Translate(move);
+
+                break;
+            case Path.DMG:
                 break;
         }
-        transform.Translate(move);
         
     }
     private void OnTriggerEnter(Collider other)
@@ -75,6 +97,30 @@ public class enemyMovement : MonoBehaviour
         {
             droids.Add(other.gameObject);
         }
+
+        if (other.gameObject.tag == "droid")
+        {
+
+            if ((other.gameObject.transform.position - transform.position).magnitude < 1.6f)
+            {
+                if (other.gameObject.GetComponent<Droid>().isBroken)
+                {
+                    enemyPath = Path.DMG;
+                    other.gameObject.GetComponent<Droid>().hacking = true;
+
+                }
+            }
+            if (other.gameObject.GetComponent<Droid>().hacked)
+            {
+                Vector3 temp = other.gameObject.transform.position;
+                Quaternion temp2 = other.gameObject.transform.rotation;
+                Destroy(other.gameObject);
+                Instantiate(enemy, temp, temp2);
+                enemyPath = defPath;
+            }
+
+        }
+
     }
     private void OnTriggerExit(Collider other)
     {
@@ -84,4 +130,20 @@ public class enemyMovement : MonoBehaviour
             
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "obstacle")
+        {
+            transform.Rotate(Vector3.up, 90f);
+        }
+        
+    }
+
+    IEnumerator Flip()
+    {
+        yield return new WaitForSeconds(runTime);
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+        avaliable = true;
+    }   
 }
